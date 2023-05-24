84e8f6b2b2fc8e40b86acafe5e7dafd687c8f599
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-05-24 09:57:57 +0300

    little update

--- backend/src/main/java/com/library/backend/controller/LibraryController.java
+++ backend/src/main/java/com/library/backend/controller/LibraryController.java
@@ -6,7 +6,6 @@ import com.library.backend.repository.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 import java.util.List;
 
 @RestController
